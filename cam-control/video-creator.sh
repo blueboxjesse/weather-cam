2cb3b599c41e100791f8b7b7c9dcf3550bf3ea03
Jesse Proudman <jesse@jesseproudman.com>
2016-01-18 16:41:29 -0800

    Create video updates

--- cam-control/video-creator.sh
+++ cam-control/video-creator.sh
@@ -4,6 +4,9 @@ source ~/.openstack-creds
 
 PROGRAM=`basename "$0"`
 
+cd ~/
+HOMEDIR=`pwd`
+
 if [ "$#" -ne 3 ]; then
   echo "$PROGRAM Usage: $PROGRAM MONTH DAY YEAR"
   exit
@@ -20,9 +23,9 @@ swift download weather-cam-$YEAR-$DAY
 
 ls *.jpg > stills.txt
 
-mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=2592:1555 -o  $YEAR-$DAY.avi -mf type=jpeg:fps=24 mf://@stills.txt
-# mencoder -nosound -ovc copy -o $YEAR-$DAY.avi -mf w=2592:h=1555:fps=18:type=jpg mf://@stills.txt
+# mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=2592:1555 -o  $YEAR-$DAY.avi -mf type=jpeg:fps=24 mf://@stills.txt
+mencoder -nosound -ovc copy -o $YEAR-$DAY.avi -mf w=2592:h=1555:fps=24:type=jpg mf://@stills.txt
 
-/usr/local/bin/youtube-upload --title "Seattle Weather Time-lapse $YEAR Day $DAY" --privacy=public --playlist="Seattle Skyline Time-lapse Recordings" --client-secrets=/home/ibmcloud/youtube-upload-settings/client_secret.json /home/ibmcloud/videos/$YEAR-$DAY/$YEAR-$DAY.avi
+/usr/local/bin/youtube-upload --title "Seattle Weather Time-lapse $YEAR Day $DAY" --privacy=public --playlist="Seattle Skyline Time-lapse Recordings" --client-secrets=$HOMEDIR/youtube_client_secret.json ~/videos/$YEAR-$DAY/$YEAR-$DAY.avi
 
 rm -rf ~/videos/$YEAR-$DAY
